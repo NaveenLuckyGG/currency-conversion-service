f8379026b32b201643d686619dd648b4b8e62d21
NaveenLuckyGG <naveengjava9@gmail.com>
2025-06-19 13:50:37 +0530

    Zipkin and Micrometer implementation

--- src/main/java/com/example/currency_conversion_service/CurrencyConversionController.java
+++ src/main/java/com/example/currency_conversion_service/CurrencyConversionController.java
@@ -3,6 +3,8 @@ package com.example.currency_conversion_service;
 import java.math.BigDecimal;
 import java.util.HashMap;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -10,12 +12,15 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.client.RestTemplate;
 
+import io.micrometer.tracing.annotation.NewSpan;
+
 @RestController
 public class CurrencyConversionController {
-	
+	Logger log=LoggerFactory.getLogger(CurrencyConversionController.class);
 	@Autowired
 	private CurrencyExchangeProxy proxy;
-
+	
+	@NewSpan
 	@GetMapping("/currency-conversion/from/{from}/to/{to}/quantity/{quantity}")
 	public CurrencyConversion calculateCurrencyConversion(@PathVariable String from, @PathVariable String to,
 			@PathVariable BigDecimal quantity) {
@@ -28,10 +33,11 @@ public class CurrencyConversionController {
 				"http://localhost:8000/currency-exchange/from/{from}/to/{to}", CurrencyConversion.class, uriVariables);
 		
 		CurrencyConversion currencyConversion = reponeEntity.getBody();
-		
+		log.info("CurrencyConversion Data" +currencyConversion);	
 		return new CurrencyConversion(currencyConversion.getId(), from, to, quantity,
 				currencyConversion.getConversionMultiple(), quantity.multiply(currencyConversion.getConversionMultiple()),
 				currencyConversion.getEnvironment() + " " + "rest template");
+		
 
 	}
 	
